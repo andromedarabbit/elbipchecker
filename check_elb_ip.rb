1acc0487267e45b186bd92b99e6c5705bdc95cc4
doublemarket <doublemarket@gmail.com>
2015-05-27 17:32:08 +0900

    変更があったときの戻り値を変更

--- check_elb_ip.rb
+++ check_elb_ip.rb
@@ -1,15 +1,20 @@
 require 'aws-sdk-core'
 require 'yaml'
+require 'optparse'
 
-LB_FILE = Dir::pwd+"/lbs.yaml"
-REGION = "ap-northeast-1"
+params = {}
+OptionParser.new do |opt|
+  opt.on("-f datafile", "File name for storing past data"){|v| params[:lb_file] = Dir::pwd+"/#{v}"}
+  opt.on("-r regionname", "Region name"){|v| params[:region] = v}
+  opt.parse!(ARGV)
+end
 
-elb = Aws::ElasticLoadBalancing::Client.new(region: REGION)
+elb = Aws::ElasticLoadBalancing::Client.new(region: params[:region])
 
-if File.exist?(LB_FILE)
-  past_lbs = YAML.load_file(LB_FILE)
+if File.exist?(params[:lb_file])
+  past_lbs = YAML.load_file(params[:lb_file])
 else
-  p "No yaml file found"
+  p "No data file found"
   past_lbs = {}
 end
 
@@ -28,9 +33,10 @@ if ! lbs.eql? past_lbs and past_lbs.length > 0
       end
     end
   end
-end
+  open(params[:lb_file], "w") do |f|
+    YAML.dump(lbs, f)
+  end
 
-open(LB_FILE, "w") do |f|
-  YAML.dump(lbs, f)
+  exit 1
 end
 
